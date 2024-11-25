791f3217db8dd7b18c16f9b97f933464da0f4437
nebulaBdj <114459629+nebulaBdj@users.noreply.github.com>
2024-11-26 00:41:42 +0900

    HOTFIX: 불필요한 Curly braces 제거
    Error: Curly braces are unnecessary here.  react/jsx-curly-brace-presence

--- src/app/home/sg-activity/layout.tsx
+++ src/app/home/sg-activity/layout.tsx
@@ -14,7 +14,7 @@ export default function SGactivityLayout({
     <div className="overflow-hidden w-full font-pretendard">
       <Script
         type="text/javascript"
-        src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=eacc39192beeb6795003897bf2fc9471&libraries=services`}
+        src='https://dapi.kakao.com/v2/maps/sdk.js?appkey=eacc39192beeb6795003897bf2fc9471&libraries=services'
         strategy="beforeInteractive"
       />
       {children}
