bb024d5fe3dddb52316b7390dd99c7ab949aff3f
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-11-25 21:24:48 +0900

    HOTFIX

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,22 @@
 const nextConfig = {
   output: 'standalone',
 
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*', 
-        destination: 'https://cnergy.p-e.kr/:path*', 
-      },
-    ]
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/api/:path*',
+  //       destination: 'https://cnergy.p-e.kr/:path*',
+  //     },
+  //   ]
+  // },
+
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_IMAGE_URL: process.env.NEXT_PUBLIC_IMAGE_URL,
+    NEXT_PUBLIC_KAKAO_API_KEY: process.env.NEXT_PUBLIC_KAKAO_API_KEY,
+    NEXT_PUBLIC_KAKAO_KEY: process.env.NEXT_PUBLIC_KAKAO_KEY,
+    NEXT_PUBLIC_MASTER_TOKEN: process.env.NEXT_PUBLIC_MASTER_TOKEN,
+    NEXT_PUBLIC_SOCIAL_LOGIN_API: process.env.NEXT_PUBLIC_SOCIAL_LOGIN_API,
   },
 }
 
