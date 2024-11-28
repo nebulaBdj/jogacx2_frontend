0d9cabacc9c4deecb0d18631304447b8d120e97d
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-11-28 22:49:49 +0900

    fix: 이미지 경로 수정

--- next.config.mjs
+++ next.config.mjs
@@ -5,10 +5,6 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/v1/:path*',
-        destination: 'https://cnergy.p-e.kr/v1/:path*',
-      },
-      {
         source: '/image/:path*',
         destination:
           'https://kr.object.ncloudstorage.com/cnergy-bucket/front_image/:path*',
