1428c0af9c3857cf1c83a54937fe962e1f566c30
yeyounging <133792082+yeyounging@users.noreply.github.com>
2024-11-28 22:26:18 +0900

    fix: 레이아웃 수정

--- src/components/MobileWrapper/MobileWrapper.tsx
+++ src/components/MobileWrapper/MobileWrapper.tsx
@@ -3,13 +3,11 @@ import SplashLogoNew from '../Icons/SplashLogoNew'
 
 export default function MobileWrapper({ children }: { children: ReactNode }) {
   return (
-    <div className="flex justify-center h-[100vh] overflow-hidden">
-      <aside className="hidden min-w-380 mr-[5%] min-[800px]:block">
-        <div className="flex flex-col h-full justify-center items-center fixed">
-          <SplashLogoNew />
-        </div>
+    <div className="flex justify-center h-screen overflow-hidden">
+      <aside className="hidden min-w-380 mr-[5%] min-[800px]:flex  items-center justify-center ">
+        <SplashLogoNew />
       </aside>
-      <div className="relative flex flex-col min-w-390 min-h-844 border-1 overflow-y-auto overflow-x-hidden scrollbar-hide my-auto">
+      <div className="min-[800px]:w-350 relative flex flex-col min-w-390 min-h-844 border-1 overflow-y-auto overflow-x-hidden scrollbar-hide my-30 rounded-10">
         {children}
       </div>
     </div>
